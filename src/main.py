b0483dc61a69ef91f475956b8652c879ab59f170
Данил <shreder.sdan@gmai.com>
2025-05-14 22:39:22 +1100

    black error

--- src/main.py
+++ src/main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(
+            Base.metadata.create_all)
 
 
 @app.on_event("shutdown")
